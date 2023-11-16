c438fc5d804e76123ec04d1f8fac0d5c0ec0e165
adenafil <nafilie9@gmail.com>
2023-11-16 13:43:39 +0700

    Belajar JFileChooser Class

---
+++ selectAFile/Main.java
@@ -0,0 +1,10 @@
+package selectAFile;
+
+public class Main {
+    public static void main(String[] args) {
+        // JFileChooser = A GUI mechanism that let a user choose a file
+        // (Helpful for opening or saving file)
+
+        new MyFrame();
+    }
+}
