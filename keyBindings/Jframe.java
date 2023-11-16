4b0e198feb8797fc9e7c5528a88d7a58851e14a8
adenafil <nafilie9@gmail.com>
2023-11-16 22:32:39 +0700

    Key Binding Class Lesson

---
+++ keyBindings/Jframe.java
@@ -0,0 +1,5 @@
+package keyBindings;
+
+public class Jframe {
+
+}
---
+++ keyBindings/Main.java
@@ -0,0 +1,14 @@
+package keyBindings;
+
+public class Main {
+    public static void main(String[] args) {
+        // Key Bindings = bind an Action to a keyStroke
+        // dont require you to click a component to give it focus
+        // all Swing components use key Bindings
+        // increased flexability compared to keyListeners
+        // can assign key storekes to individual Swing components
+        // more dificult to utilize and set uo :()
+
+        new Game();
+    }
+}
