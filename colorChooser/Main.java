9e378d54ffe5b5048d4b5ed7fa2604e72757d662
adenafil <nafilie9@gmail.com>
2023-11-16 13:56:04 +0700

    Belajar JColorChooser Class

---
+++ colorChooser/Main.java
@@ -0,0 +1,9 @@
+package colorChooser;
+
+public class Main {
+    public static void main(String[] args) {
+        // JColorChooser = A GUI mechanism that let a user choose a color
+
+        new MyFrame();
+    }
+}
---
+++ colorChooser/MyFrame.java
@@ -0,0 +1,46 @@
+package colorChooser;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MyFrame extends JFrame implements ActionListener {
+    JButton button;
+    JLabel label;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Pick A Color");
+        button.addActionListener(this);
+
+        label = new JLabel();
+        label.setBackground(Color.WHITE);
+        label.setText("This Is Some Text :)");
+        label.setFont(new Font("MV Boli", Font.PLAIN, 100));
+        label.setOpaque(true);
+
+        this.add(button);
+        this.add(label);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            JColorChooser colorChooser = new JColorChooser();
+
+            Color color = JColorChooser.showDialog(null, "Pick A Color... I Guess", Color.black);
+            label.setForeground(color);
+        }
+    }
+}
